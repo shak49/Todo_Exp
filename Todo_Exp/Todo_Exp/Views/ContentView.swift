6278ca07538d3a0a7bd92037fac836cb9beb68dc
Shak <shakfeizi@gmail.com>
2021-11-04 12:08:23 -0600

    Some UI Changes

--- Todo_Exp/Todo_Exp/Views/ContentView.swift
+++ Todo_Exp/Todo_Exp/Views/ContentView.swift
@@ -7,10 +7,39 @@
 
 import SwiftUI
 
+
+enum Priority: String, Identifiable, CaseIterable {
+    var id: UUID {
+        return UUID()
+    }
+    
+    case high = "High"
+    case medium = "Medium"
+    case low = "Low"
+}
+
+extension Priority {
+    switch self {
+    case .high:
+        return "High"
+    case .medium:
+        return "Medium"
+    case .low:
+        return "Low"
+    }
+}
+
+
 struct ContentView: View {
+    @State private var title: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                TextField("Enter a title...", text: $title)
+            }
+            .navigationTitle("To Do")
+        }
     }
 }
 
